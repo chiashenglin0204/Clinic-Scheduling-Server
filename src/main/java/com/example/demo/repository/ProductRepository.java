822220c14d2a991fb2384448c04469822cc41998
chiashenglin <bryan2160088@gmail.com>
2023-07-14 01:07:14 -0700

    RDS connection elasticbeanstalk finish

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++ src/main/java/com/example/demo/repository/ProductRepository.java
@@ -1,12 +1,9 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.demo.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-    Product findByName(String name);
-
-    
+    Product findByName(String name); 
 }
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -1,13 +1,11 @@
 package com.example.demo.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Product;
 import com.example.demo.repository.ProductRepository;
-
+import java.util.List;
 @Service
 public class ProductService {
     @Autowired
